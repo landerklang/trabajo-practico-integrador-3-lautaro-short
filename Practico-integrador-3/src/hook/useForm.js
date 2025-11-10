ce6aa3bce95025d281fee5ffa6276d69c69ce0b7
¨lautaro <shortlauty35@gmail.com>
2025-11-10 12:31:42 -0300

    se creo el custom hook useForm que luego se provara que este funcione

---
+++ Practico-integrador-3/src/hook/useForm.js
@@ -0,0 +1,20 @@
+import { useState } from "react";
+
+export const useForm = (inicialValue) => {
+  const [Form, setForm] = useState(inicialValue);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setForm({ ...Form, [name]: value });
+  };
+  const handleReset = () => {
+    setForm(inicialValue);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    handleReset();
+    console.log(Form);
+  };
+  return { Form, handleChange, handleSubmit };
+};
