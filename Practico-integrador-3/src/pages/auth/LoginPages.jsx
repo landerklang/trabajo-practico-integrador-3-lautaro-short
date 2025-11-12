8062ecafbf9d544bd0eb6c592139dcb6859de99f
¨lautaro <shortlauty35@gmail.com>
2025-11-12 16:56:35 -0300

    se realizaron las pagina de logeo y se la vinculo en la approuter

---
+++ Practico-integrador-3/src/pages/auth/LoginPages.jsx
@@ -0,0 +1,41 @@
+import { useNavigate } from "react-router-dom";
+import { useForm } from "../../hook/useForm.js";
+
+export const LoginPages = () => {
+  const navigate = useNavigate();
+  const { Form, handleChange, handleSubmit, handleReset } = useForm({
+    username: "",
+    password: "",
+  });
+
+  const handleLogin = () => {
+    logged = localStorage.getItem("isLogged", "true");
+  };
+  return (
+    <form
+      onSubmit={(event) => {
+        handleSubmit(event, navigate("/home"), handleLogin());
+      }}
+    >
+      <div>
+        <input
+          type="text"
+          name="username"
+          placeholder="nombre de usuario"
+          value={Form.username}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="password"
+          placeholder="contraseña"
+          value={Form.password}
+          onChange={handleChange}
+        />
+        <button type="submit" onChange={handleReset}>
+          submit
+        </button>
+      </div>
+    </form>
+  );
+};
--- Practico-integrador-3/src/router/AppRouter.jsx
+++ Practico-integrador-3/src/router/AppRouter.jsx
@@ -1,10 +1,25 @@
-import { Navigate, Route, Routes } from "react-router";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { PublicRoutes } from "./PublicRouter.jsx";
+import { PrivateRoutes } from "./PrivateRouter.jsx";
+import { LoginPages } from "../pages/auth/LoginPages.jsx";
 
 export const AppRouter = () => {
   return (
     <Routes>
-      <Route element />
-      <Route path="/login" element />
+      <Route element={<PublicRoutes />}>
+        <Route path="/Login" element={<LoginPages />} />
+        <Route path="/Register" element={<RegisterPages />} />
+        <Route path="*" element={<Navigate to="/Login" />} />
+        <Route path="/" element={<Navigate to="/Login" />} />
+      </Route>
+      <Route element={<PrivateRoutes />}>
+        <Route path="/Home" element={<HomePage />} />
+        <Route path="/Tasks" element />
+        <Route path="/Profile" element />
+        <Route path="*" element={<Navigate to="/Home" />} />
+        <Route path="/" element={<Navigate to="/Home" />} />
+      </Route>
+      {/* <Route path="*" element={<Navigate to="/register" />} /> */}
     </Routes>
   );
 };
